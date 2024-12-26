aa206ee9e062e57a4e1091855e1908ce06b6c3c7
231220000-Zhang San <zhangsan@foo.com>
2024-12-26 17:27:55 +0800

    1

--- nemu/src/monitor/sdb/watchpoint.h
+++ nemu/src/monitor/sdb/watchpoint.h
@@ -1,1 +1,11 @@
+#define NR_WP 32;
+
+struct {
+  int NO;
+  struct Watchpoint *next;
+  /* TODO: Add more members if necessary */
+} WP;
+
+static WP wp_pool[NR_WP] = {};
+static WP *head = NULL, *free_ = NULL;
 

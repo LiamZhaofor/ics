5ca84fa3d908bbbbf6e362a943892430c1500cf3
231220000-Zhang San <zhangsan@foo.com>
2024-12-26 17:42:45 +0800

    vim head

--- nemu/src/monitor/sdb/watchpoint.h
+++ nemu/src/monitor/sdb/watchpoint.h
@@ -1,11 +1,17 @@
 #define NR_WP 32;
 
-struct {
+typedef struct Watchpoint {
   int NO;
   struct Watchpoint *next;
   /* TODO: Add more members if necessary */
+    char *expr;
+    word_t val;
 } WP;
 
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
+
+void init_wp_pool();
+void new_wp(char *str);
+void free_wp();
 
